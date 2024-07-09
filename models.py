a51c8d4ab6995ed8f900ce5513eaf27fd4f850b6
Nixon <nokolai_polos@mail.ru>
2024-07-09 11:41:42 +0300

    add requiments

--- models.py
+++ models.py
@@ -4,21 +4,28 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy import Integer, String, Float
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 print("models.py")
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-print(f"получаем переменные окружения? - {POSTGRES_PORT}")
+try:
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-PG_DSN = (f"postgresql+asyncio://"  # +asyncio что бы sqlalchimy использовала asyncio вместо psycopg2 по умолчанию. 
-          f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
-          f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
-engine = create_async_engine(PG_DSN)  # Движок.
-Session = async_sessionmaker
+    if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT]):
+        raise ValueError("One or more required environment variables are missing.")
+
+    PG_DSN = (
+        f"postgresql+asyncio://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
+
+    engine = create_async_engine(PG_DSN)
+    Session = async_sessionmaker(engine)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 # Определение базового класса для ORM (Object-Relational Mapping) с использованием DeclarativeBase из SQLAlchemy
